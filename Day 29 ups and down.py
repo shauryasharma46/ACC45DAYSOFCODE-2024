5f314d0c04a0fc7fe3d785e19828122933178919
shauryasharma46 <145280162+shauryasharma46@users.noreply.github.com>
2024-10-31 23:28:44 +0530

    Add files via upload

---
+++ Day 29 ups and down.py
@@ -0,0 +1,11 @@
+
+for _ in range(int(input())):
+    n = int(input())
+    a =  sorted(list(map(int,input().split())))
+    ans = []
+    for i in range(n):
+        if i%2 == 0:
+            ans.append(a.pop(0))
+        else:
+            ans.append(a.pop())
+    print(*ans)        
