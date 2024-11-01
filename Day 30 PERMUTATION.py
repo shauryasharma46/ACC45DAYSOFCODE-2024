6be12373790c6c554a91075cb8daf21f493dce45
shauryasharma46 <145280162+shauryasharma46@users.noreply.github.com>
2024-11-01 16:44:59 +0530

    Add files via upload

---
+++ Day 30 PERMUTATION.py
@@ -0,0 +1,17 @@
+
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    x = list(map(int, input().split()))
+    x.sort()
+    c = 0
+    for i in range(n):
+        if x[i] > i+1:
+            print(-1)
+            break
+        elif x[i] < i+1:
+            c += (i+1 - x[i])
+    else:    
+        print(c)
